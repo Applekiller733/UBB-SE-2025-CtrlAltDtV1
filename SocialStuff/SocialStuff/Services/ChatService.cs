ba5d861f479e20626e15382afde82b28d1e9d851
albumaria <albumaria23@gmail.com>
2025-03-19 19:50:04 +0200

    Added the ChatService
    Implemented some functions in the ChatService

---
+++ SocialStuff/SocialStuff/Services/ChatService.cs
@@ -0,0 +1,81 @@
+﻿using SocialStuff.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialStuff.Model.MessageClasses;
+using SocialStuff.Model;
+
+namespace SocialStuff.Services
+{
+    class ChatService
+    {
+        private Repository repository;
+
+        public ChatService(Repository repo)
+        {
+            this.repository = repo;
+        }
+
+        public Repository getRepo()
+        {
+            return this.repository;
+        }
+
+        public void requestMoneyViaChat(List<int> ListOfRequestedUsersID, int RequesterID, float Amount, string Currency, int ChatID, string Description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void sendMoneyViaChat(List<int> ListOfReceiversID, int SenderID, float Amount, string Currency, string Description, int ChatID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void createChat(List<int> ParticipantsID, string ChatName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteChat(int ChatID)
+        {
+            this.repository.DeleteChat(ChatID);
+        }
+
+        public DateTime getLastMessageTimeStamp(int ChatID)
+        {
+            List<Message> allMessages = this.repository.GetMessagesList();
+
+            var chatMessages = allMessages.Where(m => m.getChatID() == ChatID).ToList();
+
+            var lastMessage = chatMessages.OrderByDescending(m => m.getTimestamp()).FirstOrDefault();
+
+            if (lastMessage == null)
+            {
+                throw new Exception("No messages to show in the chat with id: " + ChatID);
+            }
+
+            return lastMessage.getTimestamp();
+        }
+
+        public List<Message> getChatHistory(int ChatID)
+        {
+            List<Message> allMessages = this.repository.GetMessagesList();
+
+            List<Message> chatHistory = allMessages.Where(m => m.getChatID() == ChatID).ToList();
+
+            return chatHistory;
+        }
+
+        public void AddUserToChat(int UserID, int ChatID)
+        {
+            this.repository.AddUserToChat(UserID, ChatID);
+        }
+
+        public void RemoveUserFromChat(int UserID, int ChatID)
+        {
+            this.repository.RemoveUserFromChat(UserID, ChatID);
+        }
+    }
+}
--- SocialStuff/SocialStuff/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/SocialStuff.AssemblyInfo.cs
+++ SocialStuff/SocialStuff/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/SocialStuff.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialStuff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07a81a439a05ccd454fa9d2648d73711836923c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f57b4f7887d2c9077bc979305c9b2e56c2f0b70")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialStuff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialStuff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
