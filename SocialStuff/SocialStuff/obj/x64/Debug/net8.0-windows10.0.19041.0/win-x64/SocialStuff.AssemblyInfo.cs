8da0d9f47de19b6d4f827d50fa7e54853429c0b1
albumaria <albumaria23@gmail.com>
2025-03-19 20:00:56 +0200

    Created MessageService
    Created Message Service and implemented all of its classes

--- SocialStuff/SocialStuff/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/SocialStuff.AssemblyInfo.cs
+++ SocialStuff/SocialStuff/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/SocialStuff.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialStuff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f57b4f7887d2c9077bc979305c9b2e56c2f0b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5d861f479e20626e15382afde82b28d1e9d851")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialStuff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialStuff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
