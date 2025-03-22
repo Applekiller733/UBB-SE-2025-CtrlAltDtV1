b5c166990d3198f914a75e5e996b78f8d1546678
albumaria <albumaria23@gmail.com>
2025-03-22 17:10:12 +0200

    Added Report + Delete Button For Messages
    pfew

--- SocialStuff/SocialStuff/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/SocialStuff.AssemblyInfo.cs
+++ SocialStuff/SocialStuff/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/SocialStuff.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialStuff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a0a74947cce7179b7e2aba1ffbd8099c6e67a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315a6ca0beb1522e8acb1f6fc0969c754c84fbde")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialStuff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialStuff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
